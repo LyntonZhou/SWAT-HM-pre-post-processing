25025ea9a38f8ea53fe3d8e4c9c09e9380fa27e8
174962541 <174962541@qq.com>
2022-08-14 17:48:43 +0800

    feat: 添加rch map类型做图

--- code/python/postprocessing/analyse_output.py
+++ code/python/postprocessing/analyse_output.py
@@ -12,6 +12,8 @@ shapely.speedups.disable()
 gpd.options.use_pygeos = True
 pd.set_option('display.max_columns', 500)
 
+
+# 获取sub hru 数量
 def get_base_num():
     sub_num = 0
     hru_num = 0
@@ -25,6 +27,7 @@ def get_base_num():
     return sub_num, hru_num
 
 
+# 预处理数据主要针对hru补充年月
 def prehandle_data(data):
     if FILE_TYPE == "hru":
         _, hru_num = get_base_num()
@@ -35,22 +38,25 @@ def prehandle_data(data):
         lenth = len(data["DAY"])
         while (month_num * hru_num) < lenth:
             if data["DAY"][lenth - (month_num * hru_num + 1)] > 12:
-                now_year = data["DAY"][lenth-(month_num * hru_num + 1)]
+                now_year = data["DAY"][lenth - (month_num * hru_num + 1)]
             else:
-                start = lenth-((month_num + 1) * hru_num)
-                end = lenth-(month_num * hru_num)
+                start = lenth - ((month_num + 1) * hru_num)
+                end = lenth - (month_num * hru_num)
                 data["YEAR"][start:end] = now_year
             month_num += 1
         return data
     return data
 
 
-
+# 对不同文件类型做不同列的笛卡尔积操作
 def get_join_col():
     if FILE_TYPE == "sub":
         return "OBJECTID", "SUB"
     if FILE_TYPE == "hru":
         return "HRU_ID", "HRU"
+    if FILE_TYPE == "rch":
+        return "GRID_CODE", "RCH"
+
 
 # 地图类型做图
 def make_map_pic():
@@ -71,16 +77,48 @@ def make_map_pic():
         month = year_month[1]
         data_filted = data_output.loc[(data_output["YEAR"] == int(year)) & (data_output["MON"] == int(month)), :]
         left_on, right_on = get_join_col()
+        # join两张表
         data_merge = data_shp.merge(data_filted, left_on=left_on, right_on=right_on, how='left')
-        data_merge.plot(SELECT_COL, k=4, cmap=plt.cm.Greens, alpha=1.0, figsize=(9, 9), legend=True)  # 图形初步，如图2所示
+        data_merge.plot(SELECT_COL, k=4, cmap=plt.cm.Greens, alpha=1.0, figsize=(9, 9), legend=True)
         plt.title(f"{date}_{SELECT_COL}")
         plt.savefig(f"{OUTPUT_DIR}/{date}.png")
         print(f"save pic {OUTPUT_DIR}/{date}.png")
 
 
+# 地图类型做图
+def make_map_rch_pic():
+    # 加载数据输入数据
+    data_output = pd.read_table(INPUT_OUTPUTDATA_PATH, sep='\s+', error_bad_lines=False, index_col=False)
+    data_shp = gpd.read_file("../../../data/Shapes2/riv1.shp")
+    data_basin = gpd.read_file("../../../data/Shapes2/basin1.shp")
+    start_date = datetime.datetime.strptime(START_DATE, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(END_DATE, "%Y-%m-%d")
+    # 时间合法性判断
+    if start_date > end_date:
+        print("START_DATE can not > EDN_DATE")
+    # 获取时间范围列表
+    date_list = pd.date_range(START_DATE, END_DATE, freq='1M').strftime("%Y-%m").tolist()
+    for date in date_list:
+        year_month = date.split("-")
+        year = year_month[0]
+        month = year_month[1]
+        data_filted = data_output.loc[(data_output["YEAR"] == int(year)) & (data_output["MON"] == int(month)), :]
+        left_on, right_on = get_join_col()
+        data_merge = data_shp.merge(data_filted, left_on=left_on, right_on=right_on, how='left')
+        # 每次都创建新的工作区
+        fig, ax = plt.subplots(figsize=(10, 10))
+        ax = data_basin.plot(ax=ax, facecolor="b")
+        ax.axis('off')
+        data_merge.plot(SELECT_COL, ax=ax, k=4, cmap=plt.cm.Greens, alpha=1.0, figsize=(9, 9), legend=True)
+        plt.title(f"{date}_{SELECT_COL}")
+        fig.savefig(f"{OUTPUT_DIR}/{date}.png")
+        print(f"save pic {OUTPUT_DIR}/{date}.png")
+
+
 # 折线类型做图
 def make_line_pic():
-    data_obs = pd.read_table(INPUT_OBS_PATH, sep='\s+', header=None, error_bad_lines=False, names=["ID", "DESC", f"obs_{SELECT_COL}"])
+    data_obs = pd.read_table(INPUT_OBS_PATH, sep='\s+', header=None, error_bad_lines=False,
+                             names=["ID", "DESC", f"obs_{SELECT_COL}"])
     data_output = pd.read_table(INPUT_OUTPUTDATA_PATH, sep='\s+', error_bad_lines=False)
     data_rch = data_output.loc[(data_output["RCH"] == RCH) & (data_output["MON"] != data_output["YEAR"]), :]
     data_rch['MON'].apply(lambda x: '{:0>2d}'.format(x))
@@ -88,14 +126,17 @@ def make_line_pic():
     data_rch["DATE"] = data_rch["YEAR"].map(str) + data_rch["MON"].map(str)
     data_rch["ID"] = range(1, len(data_rch) + 1)
     m_data = data_rch.merge(data_obs, left_on='ID', right_on='ID', how='left')
-    print(m_data[SELECT_COL])
     data_merge = m_data.loc[:, [f"obs_{SELECT_COL}", SELECT_COL]]
     data_merge.plot(title=f"{RCH}_{SELECT_COL}")
     plt.savefig(f"{OUTPUT_DIR}/{RCH}_line.png")
     print(f"save pic {OUTPUT_DIR}/{RCH}_line.png")
 
 
+# map 类型的rch单独处理
 if DRAW_TYPE == "map":
-    make_map_pic()
+    if FILE_TYPE == "rch":
+        make_map_rch_pic()
+    else:
+        make_map_pic()
 elif DRAW_TYPE == "line":
     make_line_pic()
--- code/python/postprocessing/config.py
+++ code/python/postprocessing/config.py
@@ -50,21 +50,55 @@ hru示例
 '''
 rch 示例
 '''
-# 画图类型
-DRAW_TYPE = "line"
+# # 画图类型
+# DRAW_TYPE = "line"
+#
+# FILE_TYPE = "rch"
+#
+# START_DATE = "2012-01-01"
+# END_DATE = "2014-01-01"
+#
+# # shp
+# INPUT_SHP_PATH = "../../../data/Shapes2/subs1.shp"
+# INPUT_OUTPUTDATA_PATH = "../../../data/TxtInOut2/outhml.rch"
+# INPUT_OBS_PATH = "input_files/input_1.txt"
+#
+# SELECT_COL = 'DisHM_INkg'
+#
+# OUTPUT_DIR = "pic_output_rch_line"
+#
+# RCH = 1
 
+
+
+'''
+rch 地图示例
+'''
+# 画图类型 map地图 line折线
+DRAW_TYPE = "map"
+
+# 文件类型目前有rch,hru,sub 三种
 FILE_TYPE = "rch"
 
+# 做图日期范围
 START_DATE = "2012-01-01"
 END_DATE = "2014-01-01"
 
-# shp
+# 输入的shp
 INPUT_SHP_PATH = "../../../data/Shapes2/subs1.shp"
+# 如果喂rch 的map类型则需要输入basin边界shp文件
+INPUT_BASIN_SHP_PATH = "../../../data/Shapes2/basin1.shp"
+
+# 输入的输出数据文件地址
 INPUT_OUTPUTDATA_PATH = "../../../data/TxtInOut2/outhml.rch"
+# rch折线需要输入真实观测数据
 INPUT_OBS_PATH = "input_files/input_1.txt"
 
+# 需要做图的列对应INPUT_OUTPUTDATA_PATH文件中的列
 SELECT_COL = 'DisHM_INkg'
 
-OUTPUT_DIR = "pic_output_rch_line"
+# 输出图片目录 需要提前创建好
+OUTPUT_DIR = "pic_output_rch_map"
 
+# rch折线需要输入做图的rch编号
 RCH = 1
